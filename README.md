3027d5347896c8fe75eb4681013e3e0896d6e005
Boris SCHAPIRA <borisschapira@gmail.com>
2020-05-28 22:23:52 +0200

    Support analysis launch

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Dareboost Netlify Build Plugin
 
-When a build is successful, adds a Dareboost event on the page and/or User Journey monitors you have defined, using the [Dareboost API](https://www.dareboost.com/en/documentation-api).
+**You need [a Dareboost plan](https://www.dareboost.com/en/offers) to enjoy this plugin**
 
-## How to use?
+This plugin use the [Dareboost API](https://www.dareboost.com/en/documentation-api) to react to a successful build.
+
+**If your plan contains monitoring**: adds a Dareboost event on the page and/or User Journey monitors you have defined
+
+**If your plan contains API credits**: launches analysis you have configured in your `netlify.toml` file.
+
+## Minimal usage (global event)
 
 1. Install the plugin:
 
@@ -19,9 +25,9 @@ When a build is successful, adds a Dareboost event on the page and/or User Journ
 
 3. Get your Dareboost API authentication token and save it in an environment variable called DAREBOOST_API_TOKEN (under **Settings > Build & deploy > Environment > Environment variables**). Please read [Netlify's documentation about environment variables](https://docs.netlify.com/configure-builds/environment-variables/). Your API token must be kept **private**.
 
-### \[Optional\] Specify monitors
+## \[Optional\] Create an event specific to certain monitors
 
-If you want your Dareboost event to be specifically related to specific monitors, you can add some configuration using the Netlify Plugins inputs:
+If you want your Dareboost event to be related to specific monitors, you can add some configuration using the Netlify Plugins inputs:
 
 - `monitoringIds`: The ids of the page monitors relevant to the build, separated by a coma.
 - `scenarioIds`: The ids of the User Journey monitors relevant to the build, separated by a coma.
@@ -31,9 +37,37 @@ Example:
 ```
 [[plugins]]
 package = "netlify-build-plugin-dareboost"
+
   [plugins.inputs]
   monitoringIds = "7134"
   scenarioIds = "121, 122"
 ```
 
 If neither `monitoringIds` nor `scenarioIds` are defined, the event will be considered global.
+
+## \[Optional\] Launch analyses if the build is successful
+
+If you want the plugin to automatically launch analyses when the build is successful, you can define multiple analyses configurations.
+
+**Beware**: the analyses will be launched simultaneously. Make sure that your subscription supports this, otherwise just define one configuration.
+
+Example (will consume 2 API credits):
+
+```
+[[plugins]]
+package = "netlify-build-plugin-dareboost"
+
+  [plugins.inputs]
+
+    [[plugins.inputs.configurations]]
+    url = "https://boris.schapira.dev"
+    lang = "en"
+    location = "San Jose"
+    isPrivate = "true"
+    visualMetrics = "true"
+
+      [plugins.inputs.configurations.browser]
+      name = "Chrome"
+```
+
+Find more information on the multiple parameters and the API quotas in the [Dareboost API documentation](https://www.dareboost.com/en/documentation-api) (route "/analysis/launch").
--- index.js
+++ index.js
@@ -6,62 +6,85 @@ const {
     // The commit message
     COMMIT_REF,
   },
-} = require('process');
+} = require("process");
 
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+
+const callAPI = async function (route, postData, token) {
+  console.log(
+    `Dareboost: calling route "${route}". Data: ${JSON.stringify(
+      Object.assign({ token: "****************" }, postData)
+    )}`
+  );
+
+  try {
+    const { status, statusText } = await fetch(
+      `https://www.dareboost.com/api/0.6${route}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(Object.assign({ token: token }, postData)),
+      }
+    );
+
+    if (status != 200) {
+      return failPlugin(
+        `Dareboost: error while calling the route "${route}". Error: ${statusText}`
+      );
+    } else {
+      console.log(`Dareboost: calling route "${route}" => Success`);
+    }
+  } catch (error) {
+    return failPlugin(
+      `Dareboost: error while calling the route "${route}". Error:`,
+      { error }
+    );
+  }
+};
 
 module.exports = {
   async onSuccess({
     utils: {
       build: { failPlugin },
     },
-    inputs: { monitoringIds, scenarioIds },
+    inputs: { monitoringIds, scenarioIds, configurations },
   }) {
-    console.log('Dareboost: initiate event creation');
-
     if (DAREBOOST_API_TOKEN && DAREBOOST_API_TOKEN.length == 0)
       return failPlugin(
-        'Dareboost: please define your DAREBOOST_API_TOKEN as en environment variable.',
+        "Dareboost: please define your DAREBOOST_API_TOKEN as en environment variable.",
         {
           error,
         }
       );
 
-    try {
-      let postBody = {
-        token: DAREBOOST_API_TOKEN,
-        key: `build-${COMMIT_REF.slice(0, 8)}`,
-        text: `Netlify build from commit ${COMMIT_REF}`,
-        date: new Date().toISOString(),
-      };
+    let postBody = {
+      token: DAREBOOST_API_TOKEN,
+      key: `build-${COMMIT_REF.slice(0, 8)}`,
+      text: `Netlify build from commit ${COMMIT_REF}`,
+      date: new Date().toISOString(),
+    };
 
-      if (monitoringIds.length > 0) {
-        postBody.monitorings = monitoringIds
-          .split(',')
-          .map((x) => parseInt(x, 10));
-      }
-      if (scenarioIds.length > 0) {
-        postBody.scenarios = scenarioIds.split(',').map((x) => parseInt(x, 10));
-      }
+    if (monitoringIds.length > 0) {
+      postBody.monitorings = monitoringIds
+        .split(",")
+        .map((x) => parseInt(x, 10));
+    }
+    if (scenarioIds.length > 0) {
+      postBody.scenarios = scenarioIds.split(",").map((x) => parseInt(x, 10));
+    }
 
-      const { status, statusText } = await fetch(
-        'https://www.dareboost.com/api/0.6/event/create',
-        {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify(postBody),
-        }
-      );
+    await callAPI("/event/create", postBody, DAREBOOST_API_TOKEN);
 
-      if (status != 200) {
-        return failPlugin('Dareboost: event not created. Error: ' + statusText);
-      } else {
-        console.log('Dareboost: event created');
+    if (configurations.length) {
+      for (let index = 0; index < configurations.length; index++) {
+        await callAPI(
+          "/analysis/launch",
+          configurations[index],
+          DAREBOOST_API_TOKEN
+        );
       }
-    } catch (error) {
-      return failPlugin('Dareboost: error during event creation.', { error });
     }
   },
 };
